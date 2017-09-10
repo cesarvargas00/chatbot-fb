c951a26e98a0db0bbb9121ec46e1b30f61034e7c
Cesar Vargas <cesar@Cesars-MacBook-Pro.local>
2017-09-10 02:45:19 -0300

    add pug

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Post = require('../models/post');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
+	return res.json({whatisthis: 'this is a json response'});
 });
 
 module.exports = router;
