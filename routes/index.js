bc18b6aa2b1f5f7c43eed8b5755439e6dfed5b38
Cesar Vargas <cesar@Cesars-MacBook-Pro.local>
2017-09-15 04:07:08 -0300

    add post request

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,35 @@ const Post = require('../models/post');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var data = req.body;
+	if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'goosfraba') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }
+});
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  var data = req.body;
 
   // Make sure this is a page subscription
   if (data.object === 'page') {
 
     // Iterate over each entry - there may be multiple if batched
     data.entry.forEach(function(entry) {
-    	var pageID = entry.id;
-    	var timeOfEvent = entry.time;
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
 
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
-      	if (event.message) {
-      		receivedMessage(event);
-      	} else {
-      		console.log('Webhook received unknown event: ', event);
-      	}
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log('Webhook received unknown event: ', event);
+        }
       });
     });
 
@@ -39,18 +51,16 @@ function receivedMessage(event) {
   var slack = new Slack();
   slack.setWebhook('https://hooks.slack.com/services/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');
 
-	// slack emoji 
-	slack.webhook({
-		channel: '#suporte-site-criarme',
-		username: 'criarmefb',
-		icon_emoji: ':mega:',
-		text: event.message
-	}, function(err, response) {
-		console.log(response);
-	});
-	console.log('Message data: ', event.message);
+  // slack emoji 
+  slack.webhook({
+    channel: '#suporte-site-criarme',
+    username: 'criarmefb',
+    icon_emoji: ':mega:',
+    text: event.message
+  }, function(err, response) {
+    console.log(response);
+  });
+  console.log('Message data: ', event.message);
 }
-
-
 
 module.exports = router;
