61e6cdd628dcbc69b93d96b838e14eff8a0c2427
Cesar Vargas <cesar@Cesars-MacBook-Pro.local>
2017-09-15 04:00:39 -0300

    add slack integration with facebook

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,53 @@ const Post = require('../models/post');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'goosfraba') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
+	var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+    	var pageID = entry.id;
+    	var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+      	if (event.message) {
+      		receivedMessage(event);
+      	} else {
+      		console.log('Webhook received unknown event: ', event);
+      	}
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
   }
 });
+
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  var Slack = require('slack-node');
+  var slack = new Slack();
+  slack.setWebhook('https://hooks.slack.com/services/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');
+
+	// slack emoji 
+	slack.webhook({
+		channel: '#suporte-site-criarme',
+		username: 'criarmefb',
+		icon_emoji: ':mega:',
+		text: event.message
+	}, function(err, response) {
+		console.log(response);
+	});
+	console.log('Message data: ', event.message);
+}
+
+
 
 module.exports = router;
