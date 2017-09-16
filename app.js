9aac6525c939e7337ace17da85e66f4218547f08
Cesar Vargas <cesar@Cesars-MacBook-Pro.local>
2017-09-15 23:22:35 -0300

    testing some stuff on this one

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/chatbot-fb');
 
-const index = require('./routes/index');
+const index = require('./routes');
 
 const app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-// // Validation
+router.get('/', (req, res, next) => {
+  console.log('get');
+  res.json(req.body);
+});
+
+router.post('/', (req, res, next) => {
+  console.log('post');
+  res.sendStatus(200);
+});
+
+// Validation
 // router.get('/', function(req, res, next) {
 // 	if (req.query['hub.mode'] === 'subscribe' &&
 //       req.query['hub.verify_token'] === 'goosfraba') {
@@ -13,47 +23,47 @@ const router = express.Router();
 //   }
 // });
 
-//requests
-router.post('/', function(req, res, next) {
-  var data = req.body;
-  console.log('anything!');
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
+// //requests
+// router.post('/', function(req, res, next) {
+//   var data = req.body;
+//   console.log('anything!');
+//   // Make sure this is a page subscription
+//   if (data.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
+//     // Iterate over each entry - there may be multiple if batched
+//     data.entry.forEach(function(entry) {
+//       var pageID = entry.id;
+//       var timeOfEvent = entry.time;
 
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log('Webhook received unknown event: ', event);
-        }
-      });
-    });
-  }
-  res.sendStatus(200);
-});
+//       // Iterate over each messaging event
+//       entry.messaging.forEach(function(event) {
+//         if (event.message) {
+//           receivedMessage(event);
+//         } else {
+//           console.log('Webhook received unknown event: ', event);
+//         }
+//       });
+//     });
+//   }
+//   res.sendStatus(200);
+// });
 
-function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  var Slack = require('slack-node');
-  var slack = new Slack();
-  slack.setWebhook('https://hooks.slack.com/services/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');
+// function receivedMessage(event) {
+//   // Putting a stub for now, we'll expand it in the following steps
+//   var Slack = require('slack-node');
+//   var slack = new Slack();
+//   slack.setWebhook('https://hooks.slack.com/services/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');
 
-  // slack emoji 
-  slack.webhook({
-    channel: '#suporte-site-criarme',
-    username: 'criarmefb',
-    icon_emoji: ':mega:',
-    text: event.message
-  }, function(err, response) {
-    console.log(response);
-  });
-  console.log('Message data: ', event.message);
-}
+//   // slack emoji 
+//   slack.webhook({
+//     channel: '#suporte-site-criarme',
+//     username: 'criarmefb',
+//     icon_emoji: ':mega:',
+//     text: event.message
+//   }, function(err, response) {
+//     console.log(response);
+//   });
+//   console.log('Message data: ', event.message);
+// }
 
 module.exports = router;
