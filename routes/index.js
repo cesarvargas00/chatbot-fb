c6b8f5ad5964a596944d1e4e105425e253396143
Cesar Vargas <cesar@Cesars-MacBook-Pro.local>
2017-09-15 23:32:51 -0300

    fix bug on server and went back to original code

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('get');
-  res.json(req.body);
+// Validation
+router.get('/', function(req, res, next) {
+	if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'goosfraba') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }
 });
 
-router.post('/', (req, res, next) => {
-  console.log('post');
+//requests
+router.post('/', function(req, res, next) {
+  var data = req.body;
+  console.log('anything!');
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log('Webhook received unknown event: ', event);
+        }
+      });
+    });
+  }
   res.sendStatus(200);
 });
 
-// Validation
-// router.get('/', function(req, res, next) {
-// 	if (req.query['hub.mode'] === 'subscribe' &&
-//       req.query['hub.verify_token'] === 'goosfraba') {
-//     console.log("Validating webhook");
-//     res.status(200).send(req.query['hub.challenge']);
-//   } else {
-//     console.error("Failed validation. Make sure the validation tokens match.");
-//     res.sendStatus(403);          
-//   }
-// });
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  var Slack = require('slack-node');
+  var slack = new Slack();
+  slack.setWebhook('https://hooks.slack.com/services/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');
 
-// //requests
-// router.post('/', function(req, res, next) {
-//   var data = req.body;
-//   console.log('anything!');
-//   // Make sure this is a page subscription
-//   if (data.object === 'page') {
-
-//     // Iterate over each entry - there may be multiple if batched
-//     data.entry.forEach(function(entry) {
-//       var pageID = entry.id;
-//       var timeOfEvent = entry.time;
-
-//       // Iterate over each messaging event
-//       entry.messaging.forEach(function(event) {
-//         if (event.message) {
-//           receivedMessage(event);
-//         } else {
-//           console.log('Webhook received unknown event: ', event);
-//         }
-//       });
-//     });
-//   }
-//   res.sendStatus(200);
-// });
-
-// function receivedMessage(event) {
-//   // Putting a stub for now, we'll expand it in the following steps
-//   var Slack = require('slack-node');
-//   var slack = new Slack();
-//   slack.setWebhook('https://hooks.slack.com/services/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');
-
-//   // slack emoji 
-//   slack.webhook({
-//     channel: '#suporte-site-criarme',
-//     username: 'criarmefb',
-//     icon_emoji: ':mega:',
-//     text: event.message
-//   }, function(err, response) {
-//     console.log(response);
-//   });
-//   console.log('Message data: ', event.message);
-// }
+  // slack emoji 
+  slack.webhook({
+    channel: '#suporte-site-criarme',
+    username: 'criarmefb',
+    icon_emoji: ':mega:',
+    text: event.message
+  }, function(err, response) {
+    console.log(response);
+  });
+  console.log('Message data: ', event.message);
+}
 
 module.exports = router;
